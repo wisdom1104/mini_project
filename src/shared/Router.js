624d49966b855106515bb1c4b09015c8a79736c5
eh-lee <fornewstack@daum.net>
2023-03-23 01:19:24 +0900

    Router Issue Resolved Commit

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -14,39 +14,39 @@ const Router = () => {
   const isLogin = useSelector((state) => state.auth.isLogin);
 
   return (
-    <BrowserRouter>
+    //     <BrowserRouter>
 
-      <Header />
+    //       <Header />
+    //       <Routes>
+    //         <Route path="/" element={<Home />} />
+    //         <Route path="/about" element={<About />} />
+    //         <Route path="/board" element={<Board />} />
+    //         <Route path="/detail/:id" element={<Detail />} />
+    //         <Route path="/write" element={<Write />} />
+    //         <Route path="/login" element={<LogIn />} />
+    //         <Route path="/signup" element={<SignUp />} />
+    //       </Routes>
+    //     </BrowserRouter>
+    //   );
+    // };
+
+    // 요 놈 문제 있어..
+    <BrowserRouter>
+      {isLogin === true ? <Header /> : null}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/board" element={<Board />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/write" element={<Write />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        {isLogin === false ?
+          <>
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </> : null}
       </Routes>
     </BrowserRouter>
   );
 };
-
-// 요 놈 문제 있어..
-//     <BrowserRouter>
-//       {isLogin === true ? <Header /> : null}
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/board" element={<Board />} />
-//         <Route path="/detail/:id" element={<Detail />} />
-//         <Route path="/write" element={<Write />} />
-//         {isLogin === false ?
-//           <>
-//             <Route path="/login" element={<LogIn />} />
-//             <Route path="/signup" element={<SignUp />} />
-//           </> : null}
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// };
 
 export default Router;
