d666a4bc27e444453fa277705fe6773d3fb98dcf
박지혜 <jihye@bagjihyeui-MacBookAir.local>
2023-03-23 17:03:45 +0900

    css commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import { cookies } from "../shared/cookie";
 
 function About() {
@@ -13,7 +14,185 @@ function About() {
     }
   });
   // ========= LEH "Add Guard" ===========
-  return <div>About</div>;
+
+  return (
+    <EhStPage>
+      <StLayouts
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <StLayout>
+          <EhCont1
+            style={{
+              left: "0px",
+              display: "flex",
+              alignItems: "left",
+              justifyContent: "flex-start",
+            }}
+          >
+            <CopyWrapper>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  flexDirection: "row",
+                }}
+              >
+                <StyledP
+                  style={{
+                    fontWeight: "bold",
+                    color: "rgb(76, 76, 198)",
+                    WebkitTextStroke: "1px rgb(24, 24, 24)",
+                    textStroke: "1px rgb(24, 24, 24)",
+                  }}
+                >
+                  투게더
+                </StyledP>
+                <StyledP style={{ marginLeft: "0px" }}>(to-gather)는</StyledP>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  flexDirection: "row",
+                  textAlign: "right",
+                }}
+              >
+                <StyledP
+                  style={{
+                    fontWeight: "bold",
+                    color: "rgb(190, 93, 88)",
+
+                    WebkitTextStroke: "1px rgb(24, 24, 24)",
+                    textStroke: "1px rgb(24, 24, 24)",
+                  }}
+                >
+                  항해99 게더타운
+                </StyledP>
+                <StyledP style={{ marginLeft: "0px" }}>에서</StyledP>
+              </div>
+
+              <StyledP>
+                <p
+                  style={{
+                    fontSize: "100px",
+                    color: "white",
+                    textDecoration: "none",
+                    textShadow:
+                      "0px 0px 2px black, 0px 0px 2px black, 0px 0px 2px black, 0px 0px 2px black",
+                  }}
+                >
+                  일어나는 모든
+                </p>
+              </StyledP>
+
+              <StyledP style={{ textAlign: "left" }}>
+                비공식 소식을 공유하는
+              </StyledP>
+              <StyledP style={{ textAlign: "right" }}>커뮤니티입니다</StyledP>
+            </CopyWrapper>
+          </EhCont1>
+        </StLayout>
+
+        <EhCont1>
+          <CopyWrapper>
+            <StyledP
+              style={{
+                color: "rgb(90, 90, 90)",
+                fontSize: "30px",
+                textAlign: "left",
+                paddingTop: "400px",
+              }}
+            >
+              제작: 2조
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  gap: "150px",
+                }}
+              >
+                <p
+                  style={{
+                    color: "rgb(220, 220, 220)",
+                    paddingTop: "10px",
+                    paddingBottom: "10px",
+                  }}
+                >
+                  <p style={{ paddingBottom: "10px" }}>FE</p>
+                  <p>박지혜</p>
+                  <p>이은형</p>
+                </p>
+                <p
+                  style={{ color: "rgb(220, 220, 220)", paddingBottom: "10px" }}
+                >
+                  <p style={{ paddingBottom: "10px" }}>BE</p>
+                  <p>김건율</p>
+                  <p>이승빈</p>
+                  <p>장동희</p>
+                </p>
+              </div>
+            </StyledP>
+          </CopyWrapper>
+        </EhCont1>
+      </StLayouts>
+    </EhStPage>
+  );
 }
+
+const CopyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const EhCont1 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 100px;
+`;
+
+const StyledP = styled.p`
+  margin-left: 50px;
+  font-size: 50px;
+  color: white;
+  text-decoration: none;
+  text-shadow: 0px 0px 2px black, 0px 0px 2px black, 0px 0px 2px black,
+    0px 0px 2px black;
+`;
+
+const StLayout = styled.div`
+  /* background-color: gray; */
+  /* background-image: url(https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcZXn9R%2Fbtr5pa5degz%2FV2hd9LvzX1hLOKpmK9WNK0%2Fimg.png); */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  /* height: 100%;s */
+  box-sizing: border-box;
+`;
+const StLayouts = styled.div`
+  /* background-color: gray; */
+  background-image: url(https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcZXn9R%2Fbtr5pa5degz%2FV2hd9LvzX1hLOKpmK9WNK0%2Fimg.png);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: censter;
+  height: 100%;
+  box-sizing: border-box;
+`;
+
+const EhStPage = styled.div`
+  height: calc(100vh - 170px);
+  background-image: url(https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcZXn9R%2Fbtr5pa5degz%2FV2hd9LvzX1hLOKpmK9WNK0%2Fimg.png);
+  padding-top: 170px;
+`;
 
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { cookies } from "../shared/cookie";
@@ -15,29 +15,229 @@ function Home() {
   });
   // ========= LEH "Add Guard" ===========
 
+  const [showImg, setShowImg] = useState(false);
+
+  useEffect(() => {
+    setShowImg(true);
+  }, []);
+
   return (
-    <div>
-      {/* ===================== HOME PAGE ========================= */}
-      {/* <div style={{ maxHeight: "600px" }}> */}
-      {/* <HomeImg src="img/home1.jpg" /> */}
-      {/* </div> */}
-      {/* ===================== HOME PAGE ========================= */}
-      <div>
-        <button
-          onClick={() => {
-            navi(`/board`);
-          }}
-        >
-          board
-        </button>
-      </div>
-    </div>
+    <EhStPage>
+      <StLayout>
+        <EhCont1>
+          <ImgWrapper showImg={showImg}>
+            <StyledImg src="img/test1.jpg" />
+          </ImgWrapper>
+          <CopyWrapper>
+            <StyledP
+              style={{
+                marginTop: "10px",
+                fontWeight: "bold",
+                color: "rgb(190, 93, 88)",
+                WebkitTextStroke: "1px rgb(24, 24, 24)",
+                textStroke: "1px rgb(24, 24, 24)",
+              }}
+            >
+              항해99
+            </StyledP>
+
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-start",
+                flexDirection: "row",
+              }}
+            >
+              <StyledP
+                style={{
+                  fontWeight: "bold",
+                  color: "rgb(76, 76, 198)",
+                  WebkitTextStroke: "1px rgb(24, 24, 24)",
+                  textStroke: "1px rgb(24, 24, 24)",
+                }}
+              >
+                게더타운
+              </StyledP>
+
+              <p
+                style={{
+                  fontSize: "100px",
+                  color: "white",
+                  textDecoration: "none",
+                  textShadow:
+                    "0px 0px 2px black, 0px 0px 2px black, 0px 0px 2px black, 0px 0px 2px black",
+                }}
+              >
+                의
+              </p>
+            </div>
+
+            <StyledP>모든</StyledP>
+            <StyledP>비공식 소식이</StyledP>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-start",
+                flexDirection: "row",
+              }}
+            >
+              <BounceAni>
+                <StyledP
+                  style={{
+                    fontWeight: "bold",
+                    color: "rgb(40, 40, 40)",
+                    WebkitTextStroke: "2px white",
+                    textStroke: "2px white",
+                  }}
+                >
+                  한 곳
+                </StyledP>
+              </BounceAni>
+              <p
+                style={{
+                  fontSize: "100px",
+                  color: "white",
+                }}
+              >
+                에!
+              </p>
+            </div>
+          </CopyWrapper>
+        </EhCont1>
+      </StLayout>
+      {/* ======================== */}
+      <StLayout style={{ paddingTop: "150px", paddingBottom: "200px" }}>
+        <EhCont1 style={{ gap: "0px" }}>
+          <CopyWrapper style={{ textAlign: "center" }}>
+            <StyledP>여기로 ! !</StyledP>
+
+            <BounceAni>
+              <StyledP
+                style={{
+                  marginTop: "10px",
+                  fontWeight: "bold",
+                  color: "rgb(76, 76, 198)",
+                  WebkitTextStroke: "1px rgb(24, 24, 24)",
+                  textStroke: "1px rgb(24, 24, 24)",
+                }}
+              >
+                리액트
+              </StyledP>
+            </BounceAni>
+
+            <BounceAni>
+              <StyledP
+                style={{
+                  fontWeight: "bold",
+                  color: "rgb(40, 40, 40)",
+                  WebkitTextStroke: "2px white",
+                  textStroke: "2px white",
+                }}
+              >
+                노드
+              </StyledP>
+            </BounceAni>
+
+            <BounceAni>
+              <StyledP
+                style={{
+                  fontWeight: "bold",
+                  color: "rgb(190, 93, 88)",
+                  WebkitTextStroke: "1px rgb(24, 24, 24)",
+                  textStroke: "1px rgb(24, 24, 24)",
+                }}
+              >
+                스프링
+              </StyledP>
+            </BounceAni>
+
+            <StyledP>! ! 모여라</StyledP>
+          </CopyWrapper>
+          <ImgWrapper style={{ marginLeft: "100px" }} showImg={showImg}>
+            <StyledImg src="img/test3.jpg" />
+          </ImgWrapper>
+        </EhCont1>
+      </StLayout>
+    </EhStPage>
   );
 }
 
-const HomeImg = styled.img`
-  max-width: 100%;
-  max-height: 70%;
+const BounceAni = styled.div`
+  animation-name: bounce;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+  @keyframes bounce {
+    0% {
+      transform: scale(0.95);
+    }
+    50% {
+      transform: scale(1.05);
+    }
+    100% {
+      transform: scale(0.95);
+    }
+  }
+`;
+
+const CopyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+
+  opacity: 0;
+  animation: fadeIn 2s ease forwards;
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+const ImgWrapper = styled.div`
+  margin-left: 200px;
+  opacity: ${(props) => (props.showImg ? 1 : 0)};
+  transition: opacity 2s;
+`;
+
+const StyledP = styled.p`
+  margin-left: 50px;
+  font-size: 100px;
+  color: white;
+  text-decoration: none;
+  text-shadow: 0px 0px 2px black, 0px 0px 2px black, 0px 0px 2px black,
+    0px 0px 2px black;
+`;
+
+const StyledImg = styled.img`
+  border-radius: 10%;
+  border: 1px solid rgb(0, 0, 0);
+`;
+
+const EhStPage = styled.div`
+  height: 80vh;
+  background-image: url(https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcZXn9R%2Fbtr5pa5degz%2FV2hd9LvzX1hLOKpmK9WNK0%2Fimg.png);
+  padding-top: 250px;
+`;
+
+const EhCont1 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 100px;
+`;
+
+const StLayout = styled.div`
+  background-image: url(https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcZXn9R%2Fbtr5pa5degz%2FV2hd9LvzX1hLOKpmK9WNK0%2Fimg.png);
+  display: flex;
+  justify-content: center;
+  /* margin-top: 20px; */
+  padding: 20px;
+  box-sizing: border-box;
 `;
 
 export default Home;
